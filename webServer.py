ec15742d5172531b64c8a0fc288fe7e5dd10226c
aurieStudent <144954692+aurieStudent@users.noreply.github.com>
2023-09-30 08:53:38 -0400

    Update webServer.py

--- webServer.py
+++ webServer.py
@@ -55,9 +55,9 @@ def webServer(port=13331):
             # fill in end
 
             #outputdata = f.read()  #
-            outputdata = "Content-Type: text/html;\r\n"
-            outputdata2 = "Server: Apache/2.4.1 (Unix);\r\n"
-            outputdata3 = "Connection: keep-alive;\r\n"
+            outputdata = b"Content-Type: text/html;\r\n"
+            outputdata2 = b"Server: Apache/2.4.1 (Unix);\r\n"
+            outputdata3 = b"Connection: keep-alive;\r\n"
             # Fill in start -This variable can store your headers you want to send for any valid or invalid request.
             # Content-Type above is an example on how to send a header as bytes. There are more!
             # Fill in end
@@ -69,8 +69,8 @@ def webServer(port=13331):
             connectionSocket.send(bytes("HTTP/1.1 200 OK\r\n\r\n", "UTF-8"))
             #print(response.encode())
             connectionSocket.send(bytes("<html><head></head><body><h1> 200 OK </h1></body></html>\r\n\r\n", "UTF-8"))
-            connectionSocket.send(bytes("GET /helloworld.html HTTP/1.1 \r\nHost: 127.0.0.1:13331 \r\nConnection: keep-alive \r\nCache-Control: max-age=0 \r\nsec-ch-ua: Chromium  Not A;Brand Google Chrome v=116 \r\nsec-ch-ua-mobile: ?0\r\nsec-ch-ua-platform: macOS\r\nUpgrade-Insecure-Requests: 1\r\nUser-Agent: Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/116.0.0.0 Safari/537.36\r\nAccept: text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.7\r\nSec-Fetch-Site: none\r\nSec-Fetch-Mode: navigate\r\nSec-Fetch-User: ?1\r\nSec-Fetch-Dest: document\r\nAccept-Encoding: gzip, deflate, br\r\nAccept-Language: en-US,en;q=0.9\r\n\r\n", "UTF-8"))
-
+            #connectionSocket.send(bytes("GET /helloworld.html HTTP/1.1 \r\nHost: 127.0.0.1:13331 \r\nConnection: keep-alive \r\nCache-Control: max-age=0 \r\nsec-ch-ua: Chromium  Not A;Brand Google Chrome v=116 \r\nsec-ch-ua-mobile: ?0\r\nsec-ch-ua-platform: macOS\r\nUpgrade-Insecure-Requests: 1\r\nUser-Agent: Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/116.0.0.0 Safari/537.36\r\nAccept: text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.7\r\nSec-Fetch-Site: none\r\nSec-Fetch-Mode: navigate\r\nSec-Fetch-User: ?1\r\nSec-Fetch-Dest: document\r\nAccept-Encoding: gzip, deflate, br\r\nAccept-Language: en-US,en;q=0.9\r\n\r\n", "UTF-8"))
+            connectionSocket.send(bytes(message))
             #response = 'HTTP/1.1 200 OK\nConnection: close\n\n' + outputdata
             #connectionSocket.send(response.decode())
             #print(response.decode("utf-8"))
@@ -78,14 +78,12 @@ def webServer(port=13331):
             #connectionSocket.send(bytes('HTTP/1.1 200 OK\nContent-Type: text/html\n\n'))
             # Fill in end
             #connectionSocket.send(bytes(data, "UTF-8"))
-            connectionSocket.send(bytes(outputdata,"UTF-8" ))
-            connectionSocket.send(bytes(outputdata2,"UTF-8"))
-            connectionSocket.send(bytes(outputdata3,"UTF-8"))
+
             data2 = data.encode()
             message2 = message.decode()
             combined2 = message2 + data
 
-            combined = message + data2
+            combined = message + data2 + outputdata + outputdata2 + outputdata3
 
             connectionSocket.send(combined)
 
@@ -98,13 +96,13 @@ def webServer(port=13331):
                 # Fill in start - send your html file contents #Fill in end
             #connectionSocket.close()  # closing the connection socket
 
-        except IOError:
+        except Exception:
             # Send response message for invalid request due to the file not being found (404)
             # Remember the format you used in the try: block!
             # Fill in start
             #print(message.decode("HTTP/1.1 404 Not Found"))
             connectionSocket.send(bytes("HTTP/1.1 404 Not Found\r\n\r\n", "UTF-8"))
-            connectionSocket.send(bytes("<html><head></head><body><h1>404 Not Found</h1></body></html>\r\n", "UTF-8"))
+            #connectionSocket.send(bytes("<html><head></head><body><h1>404 Not Found</h1></body></html>\r\n", "UTF-8"))
             #response = 'HTTP/1.1 404 Not Found'
             #connectionSocket.send(bytes("<html><head></head><body><h1>404 Not Found</h1></body></html>\r\n", "UTF-8"))
             #print(response.encode("utf-8"))
@@ -116,8 +114,6 @@ def webServer(port=13331):
             # Fill in start
             connectionSocket.close()
             # Fill in end
-        except BrokenPipeError:
-            print('BrokenPipeError caught')
 
 
 
